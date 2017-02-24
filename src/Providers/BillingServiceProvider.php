1344dd682d76001dc69d8624b7c9b11161c7c104
Tyler Cubell <hello@tylercubell.com>
2017-02-24 16:23:59 -0500

    Remove views

--- src/Providers/BillingServiceProvider.php
+++ src/Providers/BillingServiceProvider.php
@@ -14,7 +14,6 @@ class BillingServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/../../resources/views', 'billing');
         $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
         $this->loadRoutesFrom(__DIR__.'/../../routes/webhook.php');
         $this->publishes([
